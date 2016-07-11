fb55707494bb5497fc81e85f6594f72131d9df47
Tewson Seeoun <tewson.seeoun@gmail.com>
2016-07-12 00:32:53 +0100

    Add Angular UI Router.
    Add the home module.
    
    Configure app state to default to home.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 var angular = require('angular')
 
-angular.module('app', [])
+angular.module('app',
+  [
+    require('angular-ui-router')
+  ]
+)
+  .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('app', {
+      url: '',
+      abstract: true,
+      template: '<div ui-view></div>'
+    })
+
+    $urlRouterProvider.otherwise('/home')
+  }])
   .run(function () {
     console.log('AngularJS is running.')
   })
+
+require('./home/home.routes.js')
