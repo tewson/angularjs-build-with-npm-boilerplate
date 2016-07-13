256b4e41a25fea1813d0581d47896016305552c9
Tewson Seeoun <tewson.seeoun@gmail.com>
2016-07-13 01:03:41 +0100

    Move the home page template into a template file.

--- src/home/home.routes.js
+++ src/home/home.routes.js
@@ -4,6 +4,6 @@ angular.module('app')
   .config(['$stateProvider', function ($stateProvider) {
     $stateProvider.state('app.home', {
       url: '/home',
-      template: '<div>Home</div>'
+      templateUrl: 'home/home.view.html'
     })
   }])
